8559a7aa7290fd09dd8ea2cfa2835246c4c5226c
user <user@ED06>
2020-09-29 09:47:54 +0800

    first

---
+++ src/lab1/Main.java
@@ -0,0 +1,9 @@
+package lab1;
+
+public class Main{
+	public static void main(String[] args) {
+		System.out.println("I'm tim Chung");
+		System.out.println("My nid is D0886019");
+		System.out.println("This is lab 1 hw");
+	}
+}
